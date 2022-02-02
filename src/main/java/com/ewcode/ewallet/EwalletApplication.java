bc37035d2231ac496203a552ee73a2c62abac9d1
Estevão Watanabe <estevao.watanabe@gmail.com>
2022-02-02 00:40:57 -0300

    configure rabbitmq

--- src/main/java/com/ewcode/ewallet/EwalletApplication.java
+++ src/main/java/com/ewcode/ewallet/EwalletApplication.java
@@ -1,13 +1,15 @@
 package com.ewcode.ewallet;
 
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableRabbit
 @SpringBootApplication
 public class EwalletApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EwalletApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EwalletApplication.class, args);
+    }
 
 }
